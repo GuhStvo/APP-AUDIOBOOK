2116422c1bee6d53b392b1f7b6d73c8e21cb3066
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2025-05-28 22:00:28 -0300

    AULA 28

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
 
-import StackNavigator from "./navigation/StackNavigator";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
-  return(
+  return (
     <NavigationContainer>
       <StackNavigator />
     </NavigationContainer>
-  )
+  );
 }
